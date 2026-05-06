216bc792cd363693dabf36dd15918d3fdfd4c155
Edgar Franco <edfsosa@outlook.com>
2026-05-05 22:16:37 -0300

    Añadir seeder para usuario administrador y llamar al seeder de regiones de Paraguay

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,12 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Crear un usuario administrador
         DB::table('users')->insert([
             'name' => 'Administrator',
             'email' => 'admin@example.com',
             'password' => Hash::make('password')
         ]);
+
+        // Llamar al seeder de zrkb/paraguay-regions
+        Artisan::call('paraguay-regions:seed');
     }
 }
